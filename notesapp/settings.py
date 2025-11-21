837a331c59af75dbfa49d0c6d8290777ff597ddd
bhavs09 <76781021+bhavs09@users.noreply.github.com>
2025-11-21 18:33:02 +0530

    Add IP address to ALLOWED_HOSTS in settings

--- notesapp/settings.py
+++ notesapp/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-&dzi#zsb(hz6p(s#anunt&#-a%ohr2hld71*i72*^exvw-yq$y
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*',"13.234.75.86"]
 
 
 # Application definition
